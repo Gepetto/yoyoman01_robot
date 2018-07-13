1c401e53ee22c5f72f266c43b34e0190aa4115ba
Nicolas Testard <ntestar@hagurosan.laas.fr>
2018-07-13 12:13:29 +0200

    suppression de fichiers inutiles

--- yoyoman01_traject/scripts/disable_collisions.py
+++
@@ -1,5 +0,0 @@
-
-L=["Rarm", "Larm", "RHip", "LHip", "Head", "Neck", "RKnee", "LKnee", "RFoot", "LFoot", "fixed"]
-for k in range (0,11):
- for i in range (k+1,11):
-   print("   <disable_collisions link1='"+L[k]+"' link2='"+L[i]+"' reason='Never' />")
