519108a812ed8bbd47463a91581b821ad0d6ea25
FCAM1 <fcaminad@laas.fr>
2018-10-10 17:34:29 +0200

    modif startup mvt

--- yoyoman01_traject/scripts/movement_real.py
+++ yoyoman01_traject/scripts/movement_real.py
@@ -9,16 +9,12 @@ import os
 
 
 
-
-
-
 if __name__ == "__main__":
 
   rospy.sleep(6)
 
   rospy.init_node("movement_node", anonymous=True)
   rospy.loginfo("Waiting for play_motion...")
-
 
   client = actionlib.SimpleActionClient("/play_motion", PlayMotionAction)
   rospy.loginfo("Waiting for server")
@@ -28,29 +24,40 @@ if __name__ == "__main__":
   client.wait_for_server()
   rospy.loginfo("...connected.")
 
-
   rospy.wait_for_message("joint_states", JointState)
   rospy.loginfo("joint_states get")
   rospy.sleep(1)
 
-
-  
   #launch the movement
   rospy.loginfo("movement launch")
   goal = PlayMotionGoal()
   goal.motion_name = 'home'
   goal.skip_planning = True
 
- 
 
   # Sends the goal to the action server.
-  client.send_goal(goal)
+  rospy.loginfo("Starting in 5s")
+  rospy.sleep(1)
+  rospy.loginfo("---------4")
+  rospy.sleep(1)
+  rospy.loginfo("-------3")
+  rospy.sleep(1)
+  rospy.loginfo("-----2")
+  rospy.sleep(1)
+  rospy.loginfo("--1")
+  rospy.sleep(1)
+  rospy.loginfo("GO!")
 
+  #client.send_goal(goal)
+  client.send_goal_and_wait(goal)
+  rob_state = client.get_state 	()
 
-  
-
-
+  # Wait the end of the motion
   rospy.loginfo("movement done.")
 
-while not rospy.is_shutdown():
-  i=0
+
+  #while not rospy.is_shutdown():
+    #i=0
+  exit()
+
+    
