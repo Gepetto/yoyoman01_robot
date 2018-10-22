462141ba054a3f8800722c745365ea3c2922d105
Florenc Caminade <fcaminad@bandaisan.laas.fr>
2018-10-22 10:31:51 +0200

    update path for Bandaisan

--- yoyoman01_traject/scripts/bringup_controller.py
+++ yoyoman01_traject/scripts/bringup_controller.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
   rospy.init_node('tester', anonymous=True)
   uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
   roslaunch.configure_logging(uuid)
-  launch = roslaunch.parent.ROSLaunchParent(uuid,["/home/ntestar/catkin_ws/src/yoyoman01_robot/yoyoman01_traject/launch/yoyoman01_control2.launch"])
+  launch = roslaunch.parent.ROSLaunchParent(uuid,["/home/fcaminad/Documents/GIT/catkin_ws/src/yoyoman01_robot/yoyoman01_traject/launch/yoyoman01_control2.launch"])
   launch.start()
   
   rospy.spin()
--- yoyoman01_traject/scripts/bringup_imu.py
+++ yoyoman01_traject/scripts/bringup_imu.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
   rospy.init_node('tester', anonymous=True)
   uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
   roslaunch.configure_logging(uuid)
-  launch = roslaunch.parent.ROSLaunchParent(uuid,["/home/ntestar/catkin_ws/src/yoyoman01_robot/yoyoman01_traject/launch/imu.launch"])
+  launch = roslaunch.parent.ROSLaunchParent(uuid,["/home/fcaminad/Documents/GIT/catkin_ws/src/yoyoman01_robot/yoyoman01_traject/launch/imu.launch"])
   launch.start()
   
   rospy.spin()
